608fd5f8bd728673657a4f6325e0bcae6431ab97
TheTrueTechie <league@WTS20.attlocal.net>
2017-04-27 20:00:56 -0700

    k

---
+++ src/AutocorrectTest.java
@@ -0,0 +1,29 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class AutocorrectTest {
+	@Test
+	public void testApply() {
+		String[] inputs = new String[] { "u", "you", "Youuuuu", "youtube" };
+
+		String[] outputs = new String[] { "your sister", "your sister", "your sister", "youtube" };
+
+		for (int i = 0; i < inputs.length; i++) {
+			assertEquals(String.format("Wrong result for \"%s\":", inputs[i]), outputs[i], Kata.autocorrect(inputs[i]));
+		}
+	}
+
+	@Test
+	public void testApplyv2() {
+		String[] inputs = new String[] { "I wanna hang with U", "You want to hang out?", "Youuuuu is the best!",
+				"Youtube is the best" };
+
+		String[] outputs = new String[] { "I wanna hang with your sister", "your sister want to hang out?",
+				"your sister is the best!", "Youtube is the best" };
+
+		for (int i = 0; i < inputs.length; i++) {
+			assertEquals(String.format("Wrong result for \"%s\":", inputs[i]), outputs[i], Kata.autocorrect(inputs[i]));
+		}
+	}
+}
---
+++ src/Kata.java
@@ -0,0 +1,71 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Kata {
+
+	public static String autocorrect(String inputs) {
+		String split[] = inputs.split("");
+
+		File file = new File("src/alphabetg");
+
+		try {
+
+			Scanner sc = new Scanner(file);
+
+			while (sc.hasNextLine()) {
+				String a = sc.nextLine();
+				System.out.print(a);
+
+				if (inputs.contains("y") && inputs.contains("o") && inputs.contains("u") && !inputs.contains(a)) {
+					for (int i = 0; i < split.length; i++) {
+						split[i] = "your sister";
+						inputs = split[i];
+					}
+				}
+				if (inputs.contains("Y") && inputs.contains("O") && inputs.contains("U")) {
+					for (int i = 0; i < split.length; i++) {
+						split[i] = "your sister";
+						inputs = split[i];
+					}
+				}
+				if (inputs.contains("Y") && inputs.contains("o") && inputs.contains("u")) {
+					for (int i = 0; i < split.length; i++) {
+						split[i] = "your sister";
+						inputs = split[i];
+					}
+				}
+				if (inputs.contains("y") && inputs.contains("O") && inputs.contains("u")) {
+					for (int i = 0; i < split.length; i++) {
+						split[i] = "your sister";
+						inputs = split[i];
+					}
+				}
+				if (inputs.contains("y") && inputs.contains("o") && inputs.contains("U")) {
+					for (int i = 0; i < split.length; i++) {
+						split[i] = "your sister";
+						inputs = split[i];
+					}
+				}
+				if (inputs == "u") {
+					inputs = "your sister";
+					return inputs;
+				}
+				if (inputs == "U") {
+					inputs = "your sister";
+					return inputs;
+				}
+
+				if (inputs == "youtube") {
+					return "youtube";
+				}
+			}
+
+			sc.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		return inputs;
+	}
+
+}
