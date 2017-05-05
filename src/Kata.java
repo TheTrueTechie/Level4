d58cabf6c0c5b23652870bea3a1b873a14821f25
TheTrueTechie <league@WTS20.attlocal.net>
2017-05-04 19:53:22 -0700

    bad stuff

--- src/Kata.java
+++ src/Kata.java
@@ -1,71 +1,45 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
-
 public class Kata {
 
 	public static String autocorrect(String inputs) {
-		String split[] = inputs.split("");
+		String s = "";
+		s = inputs.replaceAll("(?i)u+\\b|you+\\b", "your sister");
+		return s;
 
-		File file = new File("src/alphabetg");
-
-		try {
-
-			Scanner sc = new Scanner(file);
-
-			while (sc.hasNextLine()) {
-				String a = sc.nextLine();
-				System.out.print(a);
-
-				if (inputs.contains("y") && inputs.contains("o") && inputs.contains("u") && !inputs.contains(a)) {
-					for (int i = 0; i < split.length; i++) {
-						split[i] = "your sister";
-						inputs = split[i];
-					}
-				}
-				if (inputs.contains("Y") && inputs.contains("O") && inputs.contains("U")) {
-					for (int i = 0; i < split.length; i++) {
-						split[i] = "your sister";
-						inputs = split[i];
-					}
-				}
-				if (inputs.contains("Y") && inputs.contains("o") && inputs.contains("u")) {
-					for (int i = 0; i < split.length; i++) {
-						split[i] = "your sister";
-						inputs = split[i];
-					}
-				}
-				if (inputs.contains("y") && inputs.contains("O") && inputs.contains("u")) {
-					for (int i = 0; i < split.length; i++) {
-						split[i] = "your sister";
-						inputs = split[i];
-					}
-				}
-				if (inputs.contains("y") && inputs.contains("o") && inputs.contains("U")) {
-					for (int i = 0; i < split.length; i++) {
-						split[i] = "your sister";
-						inputs = split[i];
-					}
-				}
-				if (inputs == "u") {
-					inputs = "your sister";
-					return inputs;
-				}
-				if (inputs == "U") {
-					inputs = "your sister";
-					return inputs;
-				}
-
-				if (inputs == "youtube") {
-					return "youtube";
-				}
-			}
-
-			sc.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-		return inputs;
+		/*
+		 * String split[] = inputs.split("");
+		 * 
+		 * File file = new File("src/alphabetg");
+		 * 
+		 * try {
+		 * 
+		 * Scanner sc = new Scanner(file);
+		 * 
+		 * while (sc.hasNextLine()) { String a = sc.nextLine();
+		 * System.out.print(a);
+		 * 
+		 * if (inputs.contains("y") && inputs.contains("o") &&
+		 * inputs.contains("u") && !inputs.contains(a)) { for (int i = 0; i <
+		 * split.length; i++) { split[i] = "your sister"; inputs = split[i]; } }
+		 * if (inputs.contains("Y") && inputs.contains("O") &&
+		 * inputs.contains("U")) { for (int i = 0; i < split.length; i++) {
+		 * split[i] = "your sister"; inputs = split[i]; } } if
+		 * (inputs.contains("Y") && inputs.contains("o") &&
+		 * inputs.contains("u")) { for (int i = 0; i < split.length; i++) {
+		 * split[i] = "your sister"; inputs = split[i]; } } if
+		 * (inputs.contains("y") && inputs.contains("O") &&
+		 * inputs.contains("u")) { for (int i = 0; i < split.length; i++) {
+		 * split[i] = "your sister"; inputs = split[i]; } } if
+		 * (inputs.contains("y") && inputs.contains("o") &&
+		 * inputs.contains("U")) { for (int i = 0; i < split.length; i++) {
+		 * split[i] = "your sister"; inputs = split[i]; } } if (inputs == "u") {
+		 * inputs = "your sister"; return inputs; } if (inputs == "U") { inputs
+		 * = "your sister"; return inputs; }
+		 * 
+		 * if (inputs == "youtube") { return "youtube"; } }
+		 * 
+		 * sc.close(); } catch (FileNotFoundException e) { e.printStackTrace();
+		 * } return inputs;
+		 */
 	}
 
 }
